a10f98b33b676ada8071a2f422eb1ff9563937d4
Patrick Assaf <patrickassaf96@gmail.com>
2020-03-25 00:22:27 -0400

    Create custom hook function to manage state elements

--- src/index.js
+++ src/index.js
@@ -32,7 +32,8 @@ const PlayAgain = props => (
     </div>
 );
 
-const Game = (props) => {
+// Custom Hook
+const useGameState = () => {
     const [stars, setStars] = React.useState(utils.random(1, 9));
     const [availableNums, setAvailableNums] = React.useState(utils.range(1, 9));
     const [candidateNums, setCandidateNums] = React.useState([]);
@@ -47,6 +48,32 @@ const Game = (props) => {
         }
     });
 
+    const setGameState = (newCandidateNums) => {
+        if(utils.sum(newCandidateNums) !== stars) {
+            setCandidateNums(newCandidateNums);
+        }
+        else {
+            const newAvailableNums = availableNums.filter(
+                n => !newCandidateNums.includes(n)
+            );
+            setStars(utils.randomSumIn(newAvailableNums, 9));
+            setAvailableNums(newAvailableNums);
+            setCandidateNums([]);
+        }
+    }
+
+    return { stars, availableNums, candidateNums, secondsLeft, setGameState };
+};
+
+const Game = (props) => {
+    const {
+        stars,
+        availableNums,
+        candidateNums,
+        secondsLeft,
+        setGameState
+    } = useGameState();
+    
     const candidatesAreWrong = utils.sum(candidateNums) > stars;
     const gameStatus = availableNums.length === 0
         ? 'won'
@@ -72,17 +99,7 @@ const Game = (props) => {
                 ? candidateNums.concat(number)
                 : candidateNums.filter(cn => cn !== number);
         
-        if(utils.sum(newCandidateNums) !== stars) {
-            setCandidateNums(newCandidateNums);
-        }
-        else {
-            const newAvailableNums = availableNums.filter(
-                n => !newCandidateNums.includes(n)
-            );
-            setStars(utils.randomSumIn(newAvailableNums, 9));
-            setAvailableNums(newAvailableNums);
-            setCandidateNums([]);
-        }
+        setGameState(newCandidateNums);
     };
 
     return (
